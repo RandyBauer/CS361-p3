e03bb3525382279dec7aae87b40e0fec4239a6ec
Oliver Hill <Oliverhill@u.boisestate.edu>
2026-04-22 00:06:37 -0600

    Added Tape.java logic

--- src/tm/Tape.java
+++ src/tm/Tape.java
@@ -17,5 +17,58 @@ public class Tape {
         this.maxIndex = 0;
     }
 
+    public int read() {
+//        return tape.get(head);
+        return tape.getOrDefault(head, 0);
+    }
+
+    public void write(int symbol){
+        tape.put(head, symbol);
+        if (head < minIndex) {
+            minIndex = head;
+        }
+        if (head > maxIndex) {
+            maxIndex = head;
+        }
+    }
+
+    public void moveHead(char direction){
+        if (direction == 'R') {
+            head++;
+        }
+        else {
+            head--;
+        }
+        if (head < minIndex) {
+            minIndex = head;
+        }
+        if (head > maxIndex) {
+            maxIndex = head;
+        }
+
+    }
+
+    public void loadInput(String inputString){
+        for (int i = 0; i < inputString.length(); i++) {
+            tape.put(i, Character.getNumericValue(inputString.charAt(i)));
+            if (i < minIndex) {
+                minIndex = i;
+            }
+            if (i > maxIndex) {
+                maxIndex = i;
+            }
+        }
+    }
+
+    public String getOutput(){
+        StringBuilder strb = new StringBuilder();
+        for (int i = minIndex; i <= maxIndex; i++) {
+            strb.append(tape.getOrDefault(i, 0));
+        }
+        return strb.toString();
+    }
+
+
+
 
 }
